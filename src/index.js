196bd99ec5d8ae6f9fd973ca63dd89e66795177c
thibaultdalban <thibault.dalban@alpine-lab.com>
2020-12-01 00:15:47 +0100

    Implement locales loading

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-exports.printMsg = function () {
-  console.log("This is a message from the demo package");
+import { I18n } from "aws-amplify";
+import { Translations } from "@aws-amplify/ui-components";
+
+const LOCALES = [
+  "de",
+  "en",
+  "es",
+  "fr",
+  "pt-BR",
+  "ru",
+  "zh-hans",
+  "zh-hant",
+]
+
+export default class AmplifyI18n {
+  static configure(locales = LOCALES) {
+    locales.forEach(locale => this.loadLocale(locale));
+  }
+
+  static loadLocale(locale) {
+    if (!LOCALES.includes(locale)) return;
+    let translations = require(`./intl/${locale}.json`);
+    translations = Object
+      .entries(translations)
+      .reduce((memo, [key, content]) => {
+        key = Translations[key]
+        if (!key) return memo
+        memo[key] = content
+        return memo
+      }, {})
+    I18n.putVocabulariesForLanguage(locale, translations);
+  }
 }
