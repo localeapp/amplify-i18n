a67285559b81aca5a7708b9903fcc5e4e5ce3543
Jan-Ole Berger <jan-ole@berger-mail.de>
2021-02-25 13:16:16 +0100

    correcting locale types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-type Locale = ["ar", "de", "en", "es", "fr", "he", "it", "ja", "ko", "nl", "pt-BR", "ru", "zh-Hans", "zh-Hant"]
+type Locale = "ar" | "de" | "en" | "es" | "fr" | "he" | "it" | "ja" | "ko" | "nl" | "pt-BR" | "ru" | "zh-Hans" | "zh-Hant"
 export default class AmplifyI18n {
     static configure(locales: Locale[]): void;
     static loadLocale(locale: Locale): void;
